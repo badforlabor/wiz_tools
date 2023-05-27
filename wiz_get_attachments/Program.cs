16fbc3131057a814d1bc8e5e3d41d9c4cb11265c
liubo-work <liubo@work.com>
2023-05-27 21:19:15 +0800

    更多工具

---
+++ wiz_get_attachments/Program.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * Auth :   liubo
+ * Date :   2023-05-27 20:56:20
+ * Comment: 获取所有的附件文件
+ */
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var root = @"D:\green\WizNote\My Knowledge\Data\xxx";
+
+        var attachments = new List<string>();
+        var folders = Directory.GetDirectories(root, "*", SearchOption.AllDirectories);
+        foreach (var it in folders)
+        {
+            if (it.EndsWith("_Attachments"))
+            {
+                var files = Directory.GetFiles(it, "*", SearchOption.AllDirectories);
+                attachments.AddRange(files);
+            }
+        }
+
+        var dst = @"G:\_tempSSD\wiz_attachment";
+        foreach (var it in attachments)
+        {
+            var rel = it.Substring(root.Length + 1);
+            CopyFile(it, Path.Combine(dst, rel));
+        }
+    }
+
+    static void CopyFile(string src, string dst)
+    {
+        var folder = Path.GetDirectoryName(dst);
+        Directory.CreateDirectory(folder);
+        
+        File.Copy(src, dst, true);
+    }
+}
---
+++ wiz_zip/Program.cs
@@ -0,0 +1,63 @@
+﻿using Ionic.Zip;
+
+/**
+ * Auth :   liubo
+ * Date :   2023-05-27 20:56:20
+ * Comment: zip所有笔记文件夹
+ */
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var root = @"G:\_tempSSD\wiz";
+
+        var folders = Directory.GetDirectories(root);
+        foreach (var it in folders)
+        {
+            if (it.EndsWith("_split"))
+            {
+                continue;
+            }
+
+            try
+            {
+                File.Delete(it + ".zip");
+            }
+            catch (Exception e)
+            {
+                
+            }
+            
+            ZipFolder(it, it + ".zip");
+        }
+
+    }
+
+    public static void ZipFolder(string folder, string zipFileName)
+    {
+        if (!Directory.Exists(folder))
+        {
+            return;
+        }
+
+        try
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(zipFileName));
+        }
+        catch (Exception e)
+        {
+                
+        }
+
+        using (ZipFile zip = new ZipFile())
+        {
+            zip.UseUnicodeAsNecessary= true;  // utf-8
+            zip.AddDirectory(folder);
+            // zip.AddDirectory(folder, Path.GetFileName(folder));
+            // zip.Comment = "This zip was created at " + System.DateTime.Now.ToString("G") ; 
+            zip.Save(zipFileName);
+        }
+    }
+}
